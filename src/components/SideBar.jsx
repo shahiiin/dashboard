5eea055a3c2ba33f3c8d5523d28e109c16ffd1df
shahin <shahin.shahi1986@gmail.com>
2022-07-27 10:45:07 +0300

    provider is ready

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,7 +4,7 @@ import { SiShopware } from 'react-icons/si';
 import { MdOutlineCancel } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import { links } from '../data/dummy';
-// import { useStateContext } from '../contexts/ContextProvider';
+import { useContext } from '../context/ContextProvider';
 
 
 const SideBar = () => {
