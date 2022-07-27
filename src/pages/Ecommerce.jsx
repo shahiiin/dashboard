79837e5664cb387655e9d36291eaad1fb7b00583
shahin <shahin.shahi1986@gmail.com>
2022-07-27 14:03:46 +0300

    make card is sucsesfuly

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -41,11 +41,14 @@ const Ecommerce = () => {
                 <span className='text-lg font-semibold'>
                   {item.amount}
                 </span>
+                <span className={`text-sm text-${item.pcColor} ml-2`}>{item.percentage}</span>
               </p>
+              <p className='text-sm text-gray-400 mt-1'>{item.title}</p>
             </div>
           ))}
         </div>
       </div>
+      <div></div>
     </div>
   )
 }
